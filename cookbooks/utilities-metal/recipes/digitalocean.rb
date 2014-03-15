4b79d60d40f16f892535d41519d0a45ea67c8a61
Jonathan Hartman <j@p4nt5.com>
2014-03-14 22:35:37 -0400

    Use a temp test key just to get servers spawning

--- cookbooks/utilities-metal/recipes/digitalocean.rb
+++ cookbooks/utilities-metal/recipes/digitalocean.rb
@@ -28,7 +28,7 @@ with_fog_provisioner(
   digitalocean_api_key: ENV['DIGITALOCEAN_API_KEY']
 )
 
-fog_key_pair 'personal-dsa' do
-  public_key_path File.expand_path('~/.ssh/id_dsa.pub')
-  private_key_path File.expand_path('~/.ssh/id_dsa')
+fog_key_pair 'testkey' do
+  public_key_path '/tmp/testkey.pub'
+  private_key_path '/tmp/testkey'
 end
