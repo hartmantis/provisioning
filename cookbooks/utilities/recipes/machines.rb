1f5c8d1bad31314d4d9fe86463dcbae1d67ee17d
Jonathan Hartman <j@p4nt5.com>
2014-04-12 11:41:58 -0400

    Take a first crack at the Namecheap update code

--- cookbooks/utilities/recipes/machines.rb
+++ cookbooks/utilities/recipes/machines.rb
@@ -39,20 +39,66 @@ machine 'test1.p4nt5.com' do
   notifies :create, 'ruby_block[update_dns]'
 end
 
+# We need search; make sure we're not running as Chef Solo
+if Chef::Config[:solo]
+  Chef::Application.fatal!('This recipe requires Chef Server (or Zero)!')
+end
+
+# Make sure we have access to Namecheap at compile time
+require 'ipaddr'
+require 'namcheap'
+require 'net/http'
+
+Namecheap.configure(
+  user: ENV['NAMECHEAP_USER'],
+  api_key: ENV['NAMECHEAP_API_KEY'],
+  ip: IPAddr.new(Net::HTTP.get('icanhazip.com', '/').strip)
+)
+test = Namecheap::DNS.get_hosts('com.p4nt5')['ApiResponse']['Errors']
+test && Chef::Application.fatal!(test['Error']['__content__'])
+
 ruby_block 'update_dns' do
   block do
     require 'ipaddr'
     require 'namecheap'
     require 'net/http'
 
-    ip = search(:node, 'name:test1.p4nt5.com')[0]['ipaddress']
-    Chef::Log.info("Creating DNS entry for test1.p4nt5.com => #{ip}")
-
     Namecheap.configure(
       user: ENV['NAMECHEAP_USER'],
       api_key: ENV['NAMECHEAP_API_KEY'],
       ip: IPAddr.new(Net::HTTP.get('icanhazip.com', '/').strip)
     )
+
+    hosts = []
+    response = Namecheap::DNS.get_hosts('com.p4nt5')['ApiResponse']
+    response['CommandResponse']['DomainDNSGetHostsResult']['host'].each do |h|
+      host_hash = {
+        hostname: h['Name'],
+        type: h['Type'],
+        address: h['Address'],
+        mx_pref: h['MXPref'],
+        ttl: h['TTL']
+      }
+      hosts << host_hash
+    end
+    Chef::Log.debug("Original host records for p4nt5.com: #{hosts}")
+
+    host_ip = search(:node, 'name:test1.p4nt5.com')[0]['ipaddress']
+    Chef::Log.info("Creating DNS entry for test1.p4nt5.com => #{ip}")
+    new_host = {
+      hostname: 'test1.p4nt5.com',
+      type: 'A',
+      address: host_ip
+    }
+    hosts.delete_if do |h|
+      h['hostname'] == new_host['hostname'] && \
+        h['address'] != new_host['address']
+    end
+    hosts << new_host
+    Chef::Log.debug("New host records for p4nt5.com: #{hosts}")
+
+    # TODO: Enable once sure this is working
+    # Namecheap::DNS.set_hosts('com.p4nt5', hosts)
   end
   action :nothing
 end
